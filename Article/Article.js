e398864f6b80a168af32990d241593d76a83daa8
Ramon Audain <raudain123@gmail.com>
2019-09-04 22:53:07 -0700

    missing comma for new article

--- Article/Article.js
+++ Article/Article.js
@@ -101,7 +101,7 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
-  }
+  },
   {
     title: 'Hello Again',
     date: 'Nov 0th, 3000',
